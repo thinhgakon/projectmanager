73982435d5aabae8a99835f9ca0b60b4793b9077
thinhgakon <toi.la.dan.xu.nghe@gmail.com>
2020-09-30 17:17:00 +0700

    Logging Users In

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,39 +1,55 @@
-import React, { Component } from 'react'
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { signIn } from "./../../store/actions/authActions";
 
-class SignIn extends Component {
-    state = {
-        email: '',
-        password: ''
-    }
-    handleChange = (e) => {
-        this.setState({
-            [e.target.id]: e.target.value
-        })
-    }
-    handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(this.state);
-    }
-    render() {
-        return (
-            <div className="container">
-                <form className="white" onSubmit={this.handleSubmit}>
-                    <h5 className="grey-text text-darken-3">Sign In</h5>
-                    <div className="input-field">
-                        <label htmlFor="email">Email</label>
-                        <input type="email" id='email' onChange={this.handleChange} />
-                    </div>
-                    <div className="input-field">
-                        <label htmlFor="password">Password</label>
-                        <input type="password" id='password' onChange={this.handleChange} />
-                    </div>
-                    <div className="input-field">
-                        <button className="btn pink lighten-1 z-depth-0">Login</button>
-                    </div>
-                </form>
-            </div>
-        )
-    }
+function SignIn(props) {
+  const authError = useSelector((state) => state.auth.authError);
+  console.log("auth:", authError);
+
+  const dispatch = useDispatch();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("email:", email);
+    console.log("pass:", password);
+    dispatch(signIn({ email, password }));
+  };
+
+  return (
+    <div className="container">
+      <form className="white" onSubmit={handleSubmit}>
+        <h5 className="grey-text text-darken-3">Sign In</h5>
+        <div className="input-field">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" onChange={handleChangeEmail} />
+        </div>
+        <div className="input-field">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            onChange={handleChangePassword}
+          />
+        </div>
+        <div className="input-field">
+          <button className="btn pink lighten-1 z-depth-0">Login</button>
+          <div className="red-text center">
+            {authError ? <p>{authError}</p> : null}
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default SignIn
+export default SignIn;
