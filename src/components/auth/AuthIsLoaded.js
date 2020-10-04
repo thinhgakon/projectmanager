251c254b5f70055f9473013ad42f0a49708a37f0
thinhgakon <toi.la.dan.xu.nghe@gmail.com>
2020-10-04 12:05:40 +0700

    Waiting for Auth Ready

---
+++ src/components/auth/AuthIsLoaded.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { isLoaded } from 'react-redux-firebase';
+
+function AuthIsLoaded({ children }) {
+    const auth = useSelector(state => state.firebase.auth)
+    if (!isLoaded(auth)) return <div>splash screen...</div>;
+    return children
+}
+
+export default AuthIsLoaded;
--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import "./config/fbConfig";
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import rootReducer from './store/reducers/rootReducer';
+import AuthIsLoaded from './components/auth/AuthIsLoaded';
 
 const store = createStore(rootReducer,
   compose(
@@ -31,7 +32,9 @@ const rrfProps = {
 ReactDOM.render(
   <Provider store={store} >
     <ReactReduxFirebaseProvider {...rrfProps}>
-      <App />
+      <AuthIsLoaded>
+        <App />
+      </AuthIsLoaded>
     </ReactReduxFirebaseProvider>
   </Provider>,
   document.getElementById('root')
