7849e84c5db2a26f38e48ce1f58561ea7c06e68b
thinhgakon <toi.la.dan.xu.nghe@gmail.com>
2020-10-06 18:22:11 +0700

    Adding Projects - by author

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,11 +1,13 @@
 export const createProject = (project) => {
     return (dispatch, getState, getFirebase) => {
         // make async call to database
+        const profile = getState().firebase.profile;
+        const authorId = getState().firebase.auth.uid;
         getFirebase().firestore().collection('projects').add({
             ...project,
-            authorFirstName: 'Net',
-            authorLastName: 'Ninja',
-            authorId: 12345,
+            authorFirstName: profile.firstName,
+            authorLastName: profile.lastName,
+            authorId: authorId,
             createdAt: new Date()
         }).then(() => {
             dispatch({ type: 'CREATE_PROJECT_SUCCESS', project });
